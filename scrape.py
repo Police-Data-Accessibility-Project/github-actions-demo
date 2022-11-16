b527cbbad83f3adeb8c3df1aed6c664c0733db9b
Jacob Sanders <j.q.sanders@gmail.com>
2022-11-16 13:40:01 -0500

    make this runnable by itself

--- scrape.py
+++ scrape.py
@@ -12,16 +12,14 @@ from esridump.dumper import EsriDumper
 TARGET_LAYER = "http://gismaps.oaklandca.gov/oaklandgis/rest/services/callforservice_2015_FC/FeatureServer/0"
 
 
-def get_raw_data():
-    response = EsriDumper(TARGET_LAYER)
+def get_raw_data(target):
+    response = EsriDumper(target)
 
     return response
 
 
-def write_raw(data):
-    stub_filepath = "raw/oakland_calls_for_service"
-    filepath = f"{stub_filepath}-{date.today()}"
-    with open(filepath, "w+") as f:
+def write_raw(data, location):
+    with open(location, "w+") as f:
         json.dump(data, f, indent=4)
 
 
@@ -30,17 +28,14 @@ def write_raw(data):
 #     yield json.dumps(feature)
 
 
-def write_to_csv(data):
+def write_to_csv(data, location):
     fieldnames = [
         "type",
         "geometry",
         "properties"
     ]
 
-    filename_stub = "csv/oakland_calls_for_service"
-    filename = f"{filename_stub}-{date.today()}"
-
-    with open(filename, "w") as f:
+    with open(location, "w") as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -52,13 +47,20 @@ def write_to_csv(data):
             })
 
 
-def run_the_jewels():
-    raw = get_raw_data()
+def run_the_jewels(target, location_raw, location_csv):
+    raw = get_raw_data(target)
     # custom type in response isn't directly serializable;
     # also only want to hit the target endpoint once
     raw_as_list = list(raw)
-    write_raw(raw_as_list)
+    write_raw(raw_as_list, location_raw)
 
     # processed = (process_raw_data(feature) for feature in raw_as_list)
-    write_to_csv(raw_as_list)
-    
+    write_to_csv(raw_as_list, location_csv)
+
+
+if __name__ == "__main__":
+    stub_filename = "oakland_calls_for_service"
+    filename_raw = f"raw/{stub_filename}-{date.today()}"
+    filename_csv = f"csv/{stub_filename}-{date.today()}"
+
+    run_the_jewels(TARGET_LAYER, filename_raw, filename_csv)
